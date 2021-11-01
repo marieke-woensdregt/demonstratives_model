8af04912791c7dc292a6a9ce7e15bd68a73d018c
marieke-woensdregt <marieke.woe@gmail.com>
2021-11-01 17:40:11 +0100

    changed output filename to distinguish between Simple and RSA model

--- demonstratives_model_RSA/run_Exp1_paramsearch_pragmatic_MW.py
+++ demonstratives_model_RSA/run_Exp1_paramsearch_pragmatic_MW.py
@@ -71,7 +71,7 @@ print(output_dataframe.columns)
 
 
 output_file_path = '/Users/U968195/PycharmProjects/demonstratives_model/model_predictions/'
-output_file_name = 'HigherSearchD_MW'+'_tau_start_'+str(tau_start)+'_tau_stop_'+str(tau_stop)+'_tau_step_'+str(tau_step)+'.csv'
+output_file_name = 'HigherSearchD_MW_RSA_'+'_tau_start_'+str(tau_start)+'_tau_stop_'+str(tau_stop)+'_tau_step_'+str(tau_step)+'.csv'
 output_dataframe.to_csv(output_file_path+output_file_name, index=False)
 
 # end time:
