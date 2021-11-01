019d9db4f8d156c3d9a5c347c5137467ddd2473f
marieke-woensdregt <marieke.woe@gmail.com>
2021-11-01 15:47:54 +0100

    added code to save output in pandas dataframe

--- demonstratives_model_RSA/run_Exp1_paramsearch_pragmatic_MW.py
+++ demonstratives_model_RSA/run_Exp1_paramsearch_pragmatic_MW.py
@@ -1,11 +1,17 @@
 import LiteralSpeaker
 import PragmaticListener
-import PragmaticSpeaker
+import PragmaticSpeaker_MW
 import sys
 import numpy as np
 import itertools
+import time
+import pandas as pd
 
-itertools.chain(np.arange(0.1,0.5,0.1),np.arange(0.1,0.5,0.1))
+
+########################################################################################
+# JJE's comments:
+
+# itertools.chain(np.arange(0.1,0.5,0.1),np.arange(0.1,0.5,0.1))
 
 # LEARNED FROM SIMULATIONS:
 
@@ -13,8 +19,33 @@ itertools.chain(np.arange(0.1,0.5,0.1),np.arange(0.1,0.5,0.1))
 # FOR LISTENER: 1.1 PUTS .37 ON CLOSEST FOR 'ESTE'
 
 # FOR SPEAKER: >1 IT'S JUST CHANCE. RANGE SEEMS TO BE 0.1 TO 0.5
+########################################################################################
 
-sys.stdout.write("Model,Word,Probability,Referent,Speaker_pos,Listener_pos,Listener_att,WordNo,SpeakerTau,ListenerTau\n")
+
+########################################################################################
+# MW added code below:
+
+# starting time:
+start = time.time()
+
+tau_start = 0.1
+tau_stop = 1.2
+tau_step = 0.02
+
+output_dict = {"Model":[],
+			   "Word":[],
+			   "Cost":[],
+			   "Probability":[],
+			   "Referent":[],
+			   "Speaker_pos":[],
+			   "Listener_pos":[],
+			   "Listener_att":[],
+			   "WordNo":[],
+			   "SpeakerTau":[],
+			   "ListenerTau":[]}
+########################################################################################
+
+# sys.stdout.write("Model,Word,Probability,Referent,Speaker_pos,Listener_pos,Listener_att,WordNo,SpeakerTau,ListenerTau\n")
 
 for listener_rationality in itertools.chain(np.arange(0.01,0.1,0.01),np.arange(0.1,1.2,0.2)):
 	for speaker_rationality in np.arange(0.1,1,0.1):
@@ -25,5 +56,26 @@ for listener_rationality in itertools.chain(np.arange(0.01,0.1,0.01),np.arange(0
 					LS.SetEvent(method=method, referent=referent, lpos=lpos)
 					for words in [2,3]:
 						PL = PragmaticListener.PragmaticListener(LS,words=words)
-						PS = PragmaticSpeaker.PragmaticSpeaker(PL,referent)
+						PS = PragmaticSpeaker_MW.PragmaticSpeaker(PL,referent, output_dict)
 						PS.RunEvent()
+		output_dict = PS.output_dict  # MW: has to be updated every time, so each new speaker gets updated with the existing output_dict, and new data is written to the existing output_dict
+
+
+output_dataframe = pd.DataFrame(data=output_dict)
+pd.set_option('display.max_columns', None)
+print('')
+print('')
+print(output_dataframe)
+print('')
+print(output_dataframe.columns)
+
+
+output_file_path = '/Users/U968195/PycharmProjects/demonstratives_model/model_predictions/'
+output_file_name = 'HigherSearchD_MW'+'_tau_start_'+str(tau_start)+'_tau_stop_'+str(tau_stop)+'_tau_step_'+str(tau_step)+'.csv'
+output_dataframe.to_csv(output_file_path+output_file_name, index=False)
+
+# end time:
+end = time.time()
+
+# total time taken:
+print(f"Runtime of the program is {round(((end - start)/60), 2)} minutes")
