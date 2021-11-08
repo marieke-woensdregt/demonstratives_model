a631ceb74f121022eefccfabff116f83cc0ce98b
marieke-woensdregt <marieke.woe@gmail.com>
2021-11-08 15:02:06 +0100

    added saving pandas dataframe to pickle

--- grid_approximation.py
+++ grid_approximation.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import binom, multinomial
-import seaborn as sns
 
 
 # LOAD IN DATA: #
@@ -18,8 +17,14 @@ model = "distance"  # can be set to either "distance" or "person"
 language = "English"  # can be set to "English", "Italian", "Portuguese" or "Spanish"
 object_positions = [0, 1, 2, 3]  # array of all possible object (= referent) positions
 listener_positions = [0, 1, 2, 3]  # array of all possible listener positions
-speaker_tau = 0.4
-listener_tau = 0.4
+tau_start = 0.1
+tau_stop = 0.61
+tau_step = 0.05
+
+if language == "English" or language == "Italian":
+    data_pd = data_exp_1_two_system
+elif language == "Portuguese" or language == "Spanish":
+    data_pd = data_exp_1_three_system
 
 
 # FUNCTION DEFINITIONS: #
@@ -35,60 +40,57 @@ def calc_multinom_pmf(pd_model_predictions, pd_data, model, language, object_pos
     counts_per_word = np.zeros((WordNo))
     for i in range(len(words)):
         word = words[i]
-        print('')
-        print("word is:")
-        print(word)
-        model_prediction_row = pd_model_predictions[model_predictions["Model"]==model][model_predictions["Word"]==word][model_predictions["Referent"]==object_pos][model_predictions["Listener_pos"]==listener_pos][model_predictions["WordNo"]==WordNo][model_predictions["SpeakerTau"]==speaker_tau][model_predictions["ListenerTau"]==listener_tau]
-        print("model_prediction_row is:")
-        print(model_prediction_row)
+        model_prediction_row = pd_model_predictions[pd_model_predictions["Model"]==model][pd_model_predictions["Word"]==word][pd_model_predictions["Referent"]==object_pos][pd_model_predictions["Listener_pos"]==listener_pos][pd_model_predictions["WordNo"]==WordNo][pd_model_predictions["SpeakerTau"]==speaker_tau][pd_model_predictions["ListenerTau"]==listener_tau]
         model_prediction_prob = model_prediction_row["Probability"]
-        print("model_prediction_prob is:")
-        print(model_prediction_prob)
         probs_per_word[i] = model_prediction_prob
         # Below is object_pos+1 and listener_pos+1, because in the model_predictions dataframe it starts counting
         # from 0, but in the experimental data dataframe it starts counting from 1.
-        data_count_row = pd_data[data_exp_1_two_system["Object_Position"] == object_pos+1][data_exp_1_two_system["Listener_Position"] == listener_pos+1][data_exp_1_two_system["Language"] == language]
-        print('')
-        print("data_count_row is:")
-        print(data_count_row)
+        data_count_row = pd_data[pd_data["Object_Position"] == object_pos+1][pd_data["Listener_Position"] == listener_pos+1][pd_data["Language"] == language]
         data_count = data_count_row[word]
-        print("data_count is:")
-        print(data_count)
         counts_per_word[i] = data_count
         total = data_count_row["Total"]
-        print("total is:")
-        print(total)
     multinom_pmf = multinomial.pmf(counts_per_word, n=total, p=probs_per_word)
     multinom_logpmf = multinomial.logpmf(counts_per_word, n=total, p=probs_per_word)
-    return multinom_pmf, multinom_logpmf
+    return multinom_pmf[0], multinom_logpmf[0]
 
 
-def product_logpmf_over_situations(object_positions, listener_positions, pd_model_predictions, pd_data, model, language, speaker_tau, listener_tau):
+def product_logpmf_over_situations(pd_model_predictions, pd_data, model, language, speaker_tau, listener_tau, object_positions, listener_positions):
     logproduct = np.log(1.0)  # The first probability should be multiplied with 1.0, which is equivalent to 0.0 in log-space
     for object_pos in object_positions:
-        print('')
-        print('')
-        print("object_pos is:")
-        print(object_pos)
         for listener_pos in listener_positions:
-            print('')
-            print("listener_pos is:")
-            print(listener_pos)
             multinom_pmf, multinom_logpmf = calc_multinom_pmf(pd_model_predictions, pd_data, model, language, object_pos, listener_pos, speaker_tau, listener_tau)
-            print("multinom_pmf is:")
-            print(multinom_pmf)
-            print("multinom_logpmf is:")
-            print(multinom_logpmf)
-            print("np.exp(multinom_logpmf) is:")
-            print(np.exp(multinom_logpmf))
             logproduct += multinom_logpmf  # multiplication in probability space is equivalent to addition in log-space
     return logproduct
 
 
-logproduct = product_logpmf_over_situations(object_positions, listener_positions, model_predictions, data_exp_1_two_system, model, language, speaker_tau, listener_tau)
+def likelihood_across_parameter_settings(pd_model_predictions, pd_data, model, language, tau_start, tau_stop, tau_step, object_positions, listener_positions):
+    likelihood_dict = {"SpeakerTau":[],
+                   "ListenerTau":[],
+                   "LogLikelihood":[]}
+    for listener_rationality in np.arange(tau_start, tau_stop, tau_step):
+        print('')
+        print(f"listener_rationality is {listener_rationality}:")
+        for speaker_rationality in np.arange(tau_start, tau_stop, tau_step):
+            print(f"speaker_rationality is {speaker_rationality}:")
+            logproduct = product_logpmf_over_situations(model_predictions, data_pd, model, language, round(speaker_rationality, 2), round(listener_rationality, 2), object_positions, listener_positions)
+            print("logproduct is:")
+            print(logproduct)
+            print("np.exp(logproduct) is:")
+            print(np.exp(logproduct))
+            likelihood_dict["SpeakerTau"].append(speaker_rationality)
+            likelihood_dict["ListenerTau"].append(listener_rationality)
+            likelihood_dict["LogLikelihood"].append(logproduct)
+    likelihood_df = pd.DataFrame(data=likelihood_dict)
+    return likelihood_df
+
+
 print('')
 print('')
-print("logproduct is:")
-print(logproduct)
-print("np.exp(logproduct) is:")
-print(np.exp(logproduct))
+print(f"LANGUAGE = {language} + MODEL = {model}:")
+likelihood_df = likelihood_across_parameter_settings(model_predictions, data_pd, model, language, tau_start, tau_stop, tau_step, object_positions, listener_positions)
+print('')
+print("likelihood_df is:")
+print(likelihood_df)
+
+
+likelihood_df.to_pickle('./likelihood_df_'+language+'_'+model+'_tau_start_'+str(tau_start)+'_tau_stop_'+str(tau_stop)+'_tau_step_'+str(tau_step)+'.pkl')
