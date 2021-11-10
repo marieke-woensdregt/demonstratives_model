22a073b166b6d87dbde519a438f0bdb697e49dc5
marieke-woensdregt <marieke.woe@gmail.com>
2021-11-10 11:47:59 +0100

    added docstrings

--- plot_likelihood_heatmap.py
+++ plot_likelihood_heatmap.py
@@ -4,27 +4,35 @@ import seaborn as sns
 
 
 # PARAMETER SETTINGS: #
-model = "distance"  # can be set to either "distance" or "person"
+model = "person"  # can be set to either "distance" or "person"
 language = "Spanish"  # can be set to "English", "Italian", "Portuguese" or "Spanish"
-tau_start = 0.1
-tau_stop = 0.61
-tau_step = 0.05
+tau_start = 0.61
+tau_stop = 0.81
+tau_step = 0.02
 
-likelihood_df = pd.read_pickle('./log_likelihood_df_'+language+'_'+model+'_tau_start_'+str(tau_start)+'_tau_stop_'+str(tau_stop)+'_tau_step_'+str(tau_step)+'.pkl')
-print("likelihood_df is:")
-print(likelihood_df)
+
+log_likelihood_df = pd.read_pickle('model_fitting_data/' + 'log_likelihood_df_' + language + '_' + model + '_tau_start_' + str(tau_start) + '_tau_stop_' + str(tau_stop) + '_tau_step_' + str(tau_step) + '.pkl')
+print("LOG likelihood_df is:")
+print(log_likelihood_df)
 
 def plot_heatmap(likelihood_df):
+    """
+    Plots the (log) likelihoods of the parameters given the data in a 2D heatmap with speaker_rationality ("SpeakerTau") on the y-axis and listener_rationality ("ListenerTau") on the x-axis.
+
+    :param likelihood_df: Pandas dataframe containing (log) likelihoods. Should contain the following three columns: 'SpeakerTau', 'ListenerTau', and 'LogLikelihood'
+    :return: Saves the figure in .pdf format, and shows it
+    """
     fig, ax = plt.subplots(figsize=(11, 9))
+
+    #TODO: Should I consider replacing all -inf values in the log likelihood df with NAN? Just to make sure the heatmap is being produced on a fair scale?
+
     likelihood_df = likelihood_df.pivot(index='SpeakerTau', columns='ListenerTau', values='LogLikelihood')
-    print("likelihood_df AFTER PIVOT is:")
-    print(likelihood_df)
     sns.heatmap(likelihood_df)
     ax = sns.heatmap(likelihood_df,
                      xticklabels=likelihood_df.columns.values.round(2),
                      yticklabels=likelihood_df.index.values.round(2))
     plt.title(f"{language} + {model}")
-    plt.savefig('heatmap_log_likelihood_'+language+'_'+model+'_tau_start_'+str(tau_start)+'_tau_stop_'+str(tau_stop)+'_tau_step_'+str(tau_step)+'.pdf')
+    plt.savefig('plots/'+'heatmap_log_likelihood_'+language+'_'+model+'_tau_start_'+str(tau_start)+'_tau_stop_'+str(tau_stop)+'_tau_step_'+str(tau_step)+'.pdf')
     plt.show()
 
-plot_heatmap(likelihood_df)
+plot_heatmap(log_likelihood_df)
