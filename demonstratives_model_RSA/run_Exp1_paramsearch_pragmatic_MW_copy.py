652fc82eeb0bb8257e83a7f68fd54b4c9c86cd7c
marieke-woensdregt <marieke.woe@gmail.com>
2021-11-19 11:03:38 +0100

    added function to check which model 'wins'

---
+++ demonstratives_model_RSA/run_Exp1_paramsearch_pragmatic_MW_copy.py
@@ -0,0 +1,86 @@
+import LiteralSpeaker
+import PragmaticListener
+import PragmaticSpeaker_MW
+import sys
+import numpy as np
+import itertools
+import time
+import pandas as pd
+
+
+########################################################################################
+# JJE's comments:
+
+# itertools.chain(np.arange(0.1,0.5,0.1),np.arange(0.1,0.5,0.1))
+
+# LEARNED FROM SIMULATIONS:
+
+# FOR LISTENER: 0.1 PUTS .96 ON CLOSEST FOR 'ESTE'
+# FOR LISTENER: 1.1 PUTS .37 ON CLOSEST FOR 'ESTE'
+
+# FOR SPEAKER: >1 IT'S JUST CHANCE. RANGE SEEMS TO BE 0.1 TO 0.5
+########################################################################################
+
+
+########################################################################################
+# MW added code below:
+
+# starting time:
+start = time.time()
+
+tau_start = 0.5
+tau_stop = 10.
+tau_step = 0.5
+
+output_dict = {"Model":[],
+			   "Word":[],
+			   # "Cost":[],
+			   "Probability":[],
+			   "Referent":[],
+			   "Speaker_pos":[],
+			   "Listener_pos":[],
+			   "Listener_att":[],
+			   "WordNo":[],
+			   "SpeakerTau":[],
+			   "ListenerTau":[]}
+########################################################################################
+
+# sys.stdout.write("Model,Word,Probability,Referent,Speaker_pos,Listener_pos,Listener_att,WordNo,SpeakerTau,ListenerTau\n")
+
+# for listener_rationality in itertools.chain(np.arange(0.01,0.1,0.01),np.arange(0.1,1.2,0.2)):
+for listener_rationality in np.arange(tau_start, tau_stop, tau_step):
+	print('')
+	print(f"listener_rationality is {listener_rationality}:")
+	# for speaker_rationality in np.arange(0.1,1,0.1):
+	for speaker_rationality in np.arange(tau_start, tau_stop, tau_step):
+		print(f"speaker_rationality is {speaker_rationality}:")
+		LS = LiteralSpeaker.LiteralSpeaker(stau=speaker_rationality,ltau=listener_rationality,verbose=False) #TODO: Move the rounding to here instead of elsewhere?
+		for method in ['distance','person','pdhybrid']:
+			for lpos in [0,1,2,3]:
+				for referent in [0,1,2,3]:
+					LS.SetEvent(method=method, referent=referent, lpos=lpos)
+					for words in [2,3]:
+						PL = PragmaticListener.PragmaticListener(LS,words=words)
+						PS = PragmaticSpeaker_MW.PragmaticSpeaker(PL,referent, output_dict)
+						PS.RunEvent()
+		output_dict = PS.output_dict  # MW: has to be updated every time, so each new speaker gets updated with the existing output_dict, and new data is written to the existing output_dict
+
+
+output_dataframe = pd.DataFrame(data=output_dict)
+pd.set_option('display.max_columns', None)
+print('')
+print('')
+print(output_dataframe)
+print('')
+print(output_dataframe.columns)
+
+
+output_file_path = '/Users/U968195/PycharmProjects/demonstratives_model/model_predictions/'
+output_file_name = 'HigherSearchD_MW_RSA'+'_tau_start_'+str(tau_start)+'_tau_stop_'+str(tau_stop)+'_tau_step_'+str(tau_step)+'.csv'
+output_dataframe.to_csv(output_file_path+output_file_name, index=False)
+
+# end time:
+end = time.time()
+
+# total time taken:
+print(f"Runtime of the program is {round(((end - start)/60), 2)} minutes")
--- model_comparison.py
+++ model_comparison.py
@@ -20,6 +20,26 @@ def bayes_factor(likelihood_np_array_distance, likelihood_np_array_person):
 
 
 
+def convert_to_distance_wins(bayes_factor_df):
+    index = bayes_factor_df.index
+    column = bayes_factor_df.columns
+    bayes_factor_array = bayes_factor_df.to_numpy()
+
+    distance_wins_array = np.array(bayes_factor_array > 1)
+    person_wins_array = np.array(bayes_factor_array < 1)
+
+    distance_wins_array = distance_wins_array.astype(int)
+    person_wins_array = person_wins_array.astype(int)
+
+    for i in range(len(distance_wins_array)):
+        for j in range(len(distance_wins_array[i])):
+            if distance_wins_array[i][j] == 0 and person_wins_array[i][j] == 0:
+                distance_wins_array[i][j] = np.nan
+    distance_wins_df = pd.DataFrame(data=distance_wins_array, index=index, columns=column)
+    return distance_wins_df
+
+
+
 
 for language in languages:
     print('')
@@ -70,6 +90,12 @@ for language in languages:
     # print(likelihood_df_distance)
     # print("likelihood_df_distance.shape is:")
     # print(likelihood_df_distance.shape)
+    max_likelihood_distance = np.max(likelihood_np_array_distance)
+    print('')
+    print('')
+    print("max_likelihood_distance is:")
+    print(max_likelihood_distance)
+
 
     likelihood_np_array_person = likelihood_df_person.to_numpy()
     # print('')
@@ -78,6 +104,25 @@ for language in languages:
     # print(likelihood_df_person)
     # print("likelihood_df_person.shape is:")
     # print(likelihood_df_person.shape)
+    max_likelihood_person = np.max(likelihood_np_array_person)
+    print('')
+    print('')
+    print("max_likelihood_person is:")
+    print(max_likelihood_person)
+
+
+    if language == "English" or language == "Italian":
+        bayes_factor_max_likelihood = max_likelihood_distance / max_likelihood_person
+        print('')
+        print('')
+        print("bayes_factor_max_likelihood DISTANCE / PERSON is:")
+        print(bayes_factor_max_likelihood)
+    elif language == "Portuguese" or language == "Spanish":
+        bayes_factor_max_likelihood = max_likelihood_person / max_likelihood_distance
+        print('')
+        print('')
+        print("bayes_factor_max_likelihood PERSON / DISTANCE is:")
+        print(bayes_factor_max_likelihood)
 
 
     index = likelihood_df_person.index
@@ -106,6 +151,17 @@ for language in languages:
     print('')
     print("bayes_factor_df is:")
     print(bayes_factor_df)
+
+
+    distance_wins_df = convert_to_distance_wins(bayes_factor_df)
+    print('')
+    print('')
+    print("distance_wins_df is:")
+    print(distance_wins_df)
+
+
+    bayes_factor_df.to_pickle('model_fitting_data/' + 'bayes_factor_df_' + language + '_' + '_tau_start_' + str(
+        tau_start_for_comparison) + '_tau_stop_' + str(tau_stop) + '_tau_step_' + str(tau_step) + '.pkl')
 
 
     bayes_factor_df.to_pickle('model_fitting_data/' + 'bayes_factor_df_' + language + '_' + '_tau_start_' + str(
