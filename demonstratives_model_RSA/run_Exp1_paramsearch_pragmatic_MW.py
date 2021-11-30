465302a7f6b4e7e655c75b28eb43d1d1c1f9b37c
marieke-woensdregt <marieke.woe@gmail.com>
2021-11-30 13:46:43 +0000

    changed file path for output files

--- demonstratives_model_RSA/run_Exp1_paramsearch_pragmatic_MW.py
+++ demonstratives_model_RSA/run_Exp1_paramsearch_pragmatic_MW.py
@@ -77,7 +77,8 @@ pd.set_option('display.max_columns', None)
 # print(output_dataframe.columns)
 
 
-output_file_path = '/Users/U968195/PycharmProjects/demonstratives_model/model_predictions/'
+# output_file_path = '/Users/U968195/PycharmProjects/demonstratives_model/model_predictions/'
+output_file_path = 'pickles/'
 output_file_name = 'HigherSearchD_MW_RSA' + '_tau_start_' + str(tau_start) + '_tau_stop_' + str(
 		tau_stop) + '_tau_step_' + str(tau_step) + '.csv'
 output_dataframe.to_csv(output_file_path+output_file_name, index=False)
