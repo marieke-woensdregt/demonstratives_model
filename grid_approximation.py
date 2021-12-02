c278156f393a7fdf057533c8ba8367ecd893bfd6
marieke-woensdregt <marieke.woe@gmail.com>
2021-12-02 18:58:51 +0000

    created 'experiment 1' and 'experiment 2' versions of functions

--- grid_approximation.py
+++ grid_approximation.py
@@ -3,21 +3,28 @@ import numpy as np
 from scipy.stats import binom, multinomial
 
 # PARAMETER SETTINGS: #
-models = ["distance", "person"]
-languages = ["English", "Italian", "Portuguese", "Spanish"]
-object_positions = [0, 1, 2, 3]  # array of all possible object (= referent) positions
-listener_positions = [0, 1, 2, 3]  # array of all possible listener positions
-tau_start = 0.4
-tau_stop = 4.1
-tau_step = 0.1
-
-
 experiment = "attention"
+if experiment == "attention":
+    models = ["distance_attention", "person_attention"]
+else:
+    models = ["distance", "person"]
+languages = ["English", "Italian", "Portuguese", "Spanish"]
+if experiment == "attention":
+    object_positions = [1, 2, 3]  # array of all possible object (= referent) positions
+else:
+    object_positions = [0, 1, 2, 3]  # array of all possible object (= referent) positions
+listener_positions = [0, 1, 2, 3]  # array of all possible listener positions
+listener_attentions = [0, 1, 2, 3]  # array of all possible listener positions
+tau_start = 0.4
+tau_stop = 2.02
+tau_step = 0.02
+
+
 
 
 # FUNCTION DEFINITIONS: #
 
-def calc_multinom_pmf(pd_model_predictions, pd_data, model, language, object_pos, listener_pos, speaker_tau, listener_tau):
+def calc_multinom_pmf(experiment, pd_model_predictions, pd_data, model, language, speaker_tau, listener_tau, object_pos, listener_pos=None, listener_att=None):
     """
     Calculates the likelihood and log_likelihood of the parameters speaker_tau and listener_tau given the data.
 
@@ -38,24 +45,64 @@ def calc_multinom_pmf(pd_model_predictions, pd_data, model, language, object_pos
         WordNo = 3
         words = ["este", "ese", "aquel"]
     probs_per_word = np.zeros((WordNo))
+
     counts_per_word = np.zeros((WordNo))
+    # counts_per_word = []
+
     for i in range(len(words)):
         word = words[i]
-        model_prediction_row = pd_model_predictions[pd_model_predictions["Model"]==model][pd_model_predictions["Word"]==word][pd_model_predictions["Referent"]==object_pos][pd_model_predictions["Listener_pos"]==listener_pos][pd_model_predictions["WordNo"]==WordNo][pd_model_predictions["SpeakerTau"]==speaker_tau][pd_model_predictions["ListenerTau"]==listener_tau]
+        if experiment == "attention":
+            model_prediction_row = pd_model_predictions[pd_model_predictions["Model"]==model][pd_model_predictions["Word"]==word][pd_model_predictions["Referent"]==object_pos][pd_model_predictions["Listener_att"]==listener_att][pd_model_predictions["WordNo"]==WordNo][pd_model_predictions["SpeakerTau"]==speaker_tau][pd_model_predictions["ListenerTau"]==listener_tau]
+        else:
+            model_prediction_row = pd_model_predictions[pd_model_predictions["Model"]==model][pd_model_predictions["Word"]==word][pd_model_predictions["Referent"]==object_pos][pd_model_predictions["Listener_pos"]==listener_pos][pd_model_predictions["WordNo"]==WordNo][pd_model_predictions["SpeakerTau"]==speaker_tau][pd_model_predictions["ListenerTau"]==listener_tau]
         model_prediction_prob = model_prediction_row["Probability"]
         probs_per_word[i] = model_prediction_prob
         # Below is object_pos+1 and listener_pos+1, because in the model_predictions dataframe it starts counting
         # from 0, but in the experimental data dataframe it starts counting from 1.
-        data_count_row = pd_data[pd_data["Object_Position"] == object_pos+1][pd_data["Listener_Position"] == listener_pos+1][pd_data["Language"] == language]
-        data_count = data_count_row[word]
-        counts_per_word[i] = data_count
+        if experiment == "attention":
+            data_count_row = pd_data[pd_data["Object_Position"] == object_pos+1][pd_data["Listener_Attention"] == listener_att+1][pd_data["Language"] == language]
+            # print('')
+            # print("data_count_row is:")
+            # print(data_count_row)
+        else:
+            data_count_row = pd_data[pd_data["Object_Position"] == object_pos+1][pd_data["Listener_Position"] == listener_pos+1][pd_data["Language"] == language]
+        if experiment == "attention":
+            data_count = data_count_row[word.title()]
+            # print('')
+            # print("data_count is:")
+            # print(data_count)
+            # print("len(data_count) is:")
+            # print(len(data_count))
+        else:
+            data_count = data_count_row[word]
+
+        if len(data_count) > 1:
+            data_count = np.mean(data_count)  #TODO: Check whether this is really the right way to handle this.
+            # print("data_count AFTER AVERAGING is:")
+            # print(data_count)
+            counts_per_word[i] = data_count
+            # counts_per_word.append(data_count)
+        else:
+            counts_per_word[i] = data_count
+            # counts_per_word.append(data_count)
+
+        # print('')
+        # print("counts_per_word is:")
+        # print(counts_per_word)
+        # print("len(counts_per_word) is:")
+        # print(len(counts_per_word))
+        # print("len(counts_per_word[0]) is:")
+        # print(len(counts_per_word[0]))
+        # print("len(counts_per_word[1]) is:")
+        # print(len(counts_per_word[1]))
+
         total = data_count_row["Total"]
     multinom_logpmf = multinomial.logpmf(counts_per_word, n=total, p=probs_per_word)
     multinom_pmf = multinomial.pmf(counts_per_word, n=total, p=probs_per_word)
     return multinom_logpmf[0], multinom_pmf[0]
 
 
-def product_logpmf_over_situations(pd_model_predictions, pd_data, model, language, speaker_tau, listener_tau, object_positions, listener_positions):
+def product_logpmf_over_situations(experiment, pd_model_predictions, pd_data, model, language, speaker_tau, listener_tau, object_positions, listener_positions=None, listener_attentions=None):
     """
     Calculates the product of the likelihood values (i.e. sum of the LOG likelihood values) across all situations (i.e. all different trials) in the experiment. That is: all different combinations of possible object_positions and listener_positions.
 
@@ -71,16 +118,24 @@ def product_logpmf_over_situations(pd_model_predictions, pd_data, model, languag
     """
     log_product = np.log(1.0)  # The first probability should be multiplied with 1.0, which is equivalent to 0.0 in log-space
     product = 1.0
-    for object_pos in object_positions:
-        for listener_pos in listener_positions:
-            multinom_logpmf, multinom_pmf = calc_multinom_pmf(pd_model_predictions, pd_data, model, language, object_pos, listener_pos, speaker_tau, listener_tau)
-            #TODO: Should I be using logaddexp() or logsumexp() instead?
-            log_product += multinom_logpmf  # multiplication in probability space is equivalent to addition in log-space
-            product *= multinom_pmf
+    if experiment == "attention":
+        for object_pos in object_positions:
+            for listener_att in listener_attentions:
+                multinom_logpmf, multinom_pmf = calc_multinom_pmf(experiment, pd_model_predictions, pd_data, model, language, speaker_tau, listener_tau, object_pos, listener_att=listener_att)
+                #TODO: Should I be using logaddexp() or logsumexp() instead?
+                log_product += multinom_logpmf  # multiplication in probability space is equivalent to addition in log-space
+                product *= multinom_pmf
+    else:
+        for object_pos in object_positions:
+            for listener_pos in listener_positions:
+                multinom_logpmf, multinom_pmf = calc_multinom_pmf(experiment, pd_model_predictions, pd_data, model, language, speaker_tau, listener_tau, object_pos, listener_pos=listener_pos)
+                #TODO: Should I be using logaddexp() or logsumexp() instead?
+                log_product += multinom_logpmf  # multiplication in probability space is equivalent to addition in log-space
+                product *= multinom_pmf
     return log_product, product
 
 
-def likelihood_across_parameter_settings(pd_model_predictions, pd_data, model, language, tau_start, tau_stop, tau_step, object_positions, listener_positions):
+def likelihood_across_parameter_settings(experiment, pd_model_predictions, pd_data, model, language, tau_start, tau_stop, tau_step, object_positions, listener_positions=None, listener_attentions=None):
     """
     Calculates the sum of LOG likelihoods and product of likelihoods of the parameters given the data across a range of combinations of possible parameter settings for speaker_rationality (i.e. SpeakerTau) and listener_rationality (i.e. ListenerTau).
 
@@ -106,7 +161,10 @@ def likelihood_across_parameter_settings(pd_model_predictions, pd_data, model, l
         print(f"listener_rationality is {listener_rationality}:")
         for speaker_rationality in np.arange(tau_start, tau_stop, tau_step):
             print(f"speaker_rationality is {speaker_rationality}:")
-            log_product, prob_product = product_logpmf_over_situations(pd_model_predictions, pd_data, model, language, round(speaker_rationality, 2), round(listener_rationality, 2), object_positions, listener_positions)
+            if experiment == "attention":
+                log_product, prob_product = product_logpmf_over_situations(experiment, pd_model_predictions, pd_data, model, language, round(speaker_rationality, 2), round(listener_rationality, 2), object_positions, listener_attentions=listener_attentions)
+            else:
+                log_product, prob_product = product_logpmf_over_situations(experiment, pd_model_predictions, pd_data, model, language, round(speaker_rationality, 2), round(listener_rationality, 2), object_positions, listener_positions=listener_positions)
             log_likelihood_dict["SpeakerTau"].append(speaker_rationality)
             log_likelihood_dict["ListenerTau"].append(listener_rationality)
             log_likelihood_dict["LogLikelihood"].append(log_product)
@@ -134,15 +192,15 @@ for language in languages:
     if experiment == "attention":
 
         if language == "English" or language == "Italian":
-            data_pd = pd.read_csv('data/with_counts/TwoSystem.csv', index_col=0)
+            data_pd = pd.read_csv('data/experiment_2/with_counts/TwoSystem_Attention.csv', index_col=0)
         elif language == "Portuguese" or language == "Spanish":
-            data_pd = pd.read_csv('data/with_counts/ThreeSystem.csv', index_col=0)
+            data_pd = pd.read_csv('data/experiment_2/with_counts/ThreeSystem_Attention.csv', index_col=0)
 
     else:
         if language == "English" or language == "Italian":
-            data_pd = pd.read_csv('data/with_counts/TwoSystem.csv', index_col=0)
+            data_pd = pd.read_csv('data/experiment_1/with_counts/TwoSystem.csv', index_col=0)
         elif language == "Portuguese" or language == "Spanish":
-            data_pd = pd.read_csv('data/with_counts/ThreeSystem.csv', index_col=0)
+            data_pd = pd.read_csv('data/experiment_1/with_counts/ThreeSystem.csv', index_col=0)
 
     for model in models:
         print('')
@@ -151,7 +209,10 @@ for language in languages:
         print('')
         print('')
         print(f"LANGUAGE = {language} + MODEL = {model}:")
-        log_likelihood_df, likelihood_df = likelihood_across_parameter_settings(model_predictions, data_pd, model, language, tau_start, tau_stop, tau_step, object_positions, listener_positions)
+        if experiment == "attention":
+            log_likelihood_df, likelihood_df = likelihood_across_parameter_settings(experiment, model_predictions, data_pd, model, language, tau_start, tau_stop, tau_step, object_positions, listener_attentions=listener_attentions)
+        else:
+            log_likelihood_df, likelihood_df = likelihood_across_parameter_settings(experiment, model_predictions, data_pd, model, language, tau_start, tau_stop, tau_step, object_positions, listener_positions=listener_positions)
         print('')
         print('')
         print("log_likelihood_df is:")
