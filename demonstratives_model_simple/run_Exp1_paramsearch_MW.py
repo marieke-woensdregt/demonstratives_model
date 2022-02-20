497edf78629a7b87006954fd6fedae6c637b738e
marieke-woensdregt <marieke.woe@gmail.com>
2022-02-20 01:14:22 +0100

    changed file/folder names for computer cluster

--- demonstratives_model_simple/run_Exp1_paramsearch_MW.py
+++ demonstratives_model_simple/run_Exp1_paramsearch_MW.py
@@ -12,9 +12,11 @@ speaker_rationality = 2.
 ########################################################################################
 # MW added code below:
 
-tau_start = 0.1
-tau_stop = 1.2
-tau_step = 0.02
+tau_start = 0.4
+tau_stop = 2.05
+tau_step = 0.05
+
+models = ['distance', 'person']  # ['distance', 'person'] # ['distance_attention', 'person_attention']  # Can contain: 'distance','person','pdhybrid', 'distance_attention', 'person_attention'
 
 output_dict = {"Model":[],
 			   "Word":[],
@@ -34,16 +36,16 @@ Model = Speaker_MW.Speaker(output_dict, stau=speaker_rationality, ltau=listener_
 # Model.PrintHeader()  # MW: Got rid of this, because everything is being saved to a pandas dataframe now.
 
 for listener_rationality in np.arange(tau_start, tau_stop, tau_step):
-	print('')
-	print(f"listener_rationality is {listener_rationality}:")
+	# print('')
+	# print(f"listener_rationality is {listener_rationality}:")
 	for speaker_rationality in np.arange(tau_start, tau_stop, tau_step):
-		print(f"speaker_rationality is {speaker_rationality}:")
+		# print(f"speaker_rationality is {speaker_rationality}:")
 		Model = Speaker_MW.Speaker(output_dict, stau=speaker_rationality,ltau=listener_rationality,verbose=False)
-		for method in ['distance','person','pdhybrid']:
+		for model in models:
 			for lpos in [0,1,2,3]:
 				for referent in [0,1,2,3]:
 					for words in [2,3]:
-						Model.SetEvent(method=method, referent=referent, lpos=lpos)
+						Model.SetEvent(method=model, referent=referent, lpos=lpos)
 						Model.RunEvent(words=words,header=False)
 		output_dict = Model.output_dict # MW: has to be updated every time, so each new speaker gets updated with the existing output_dict, and new data is written to the existing output_dict
 
@@ -51,16 +53,18 @@ for listener_rationality in np.arange(tau_start, tau_stop, tau_step):
 
 output_dataframe = pd.DataFrame(data=output_dict)
 pd.set_option('display.max_columns', None)
-print('')
-print('')
-print(output_dataframe)
-print('')
-print(output_dataframe.columns)
+# print('')
+# print('')
+# print(output_dataframe)
+# print('')
+# print(output_dataframe.columns)
 
 
-output_file_path = '/Users/U968195/PycharmProjects/demonstratives_model/model_predictions/'
-output_file_name = 'HigherSearchD_MW_Simple_'+'_tau_start_'+str(tau_start)+'_tau_stop_'+str(tau_stop)+'_tau_step_'+str(tau_step)+'.csv'
+# output_file_path = '/Users/U968195/PycharmProjects/demonstratives_model/model_predictions/'
+output_file_path = 'pickles/'
+output_file_name = 'HigherSearchD_MW_Simple_'+ str(models).replace(" ", "") + '_tau_start_'+str(tau_start)+'_tau_stop_'+str(tau_stop)+'_tau_step_'+str(tau_step)+'.csv'
 output_dataframe.to_csv(output_file_path+output_file_name, index=False)
+
 
 # end time:
 end = time.time()
