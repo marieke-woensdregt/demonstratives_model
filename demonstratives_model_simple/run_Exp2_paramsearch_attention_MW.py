587a31da12787d4d57d3a0a37f87f81ac78c8a48
marieke-woensdregt <marieke.woe@gmail.com>
2022-02-21 19:35:41 +0100

    added uniform_ese parameter

--- demonstratives_model_simple/run_Exp2_paramsearch_attention_MW.py
+++ demonstratives_model_simple/run_Exp2_paramsearch_attention_MW.py
@@ -18,6 +18,8 @@ tau_step = 0.05
 
 models = ['distance_attention', 'person_attention']  # ['distance', 'person'] # ['distance_attention', 'person_attention']  # Can contain: 'distance','person','pdhybrid', 'distance_attention', 'person_attention'
 
+ese_uniform = True  # Can be set to either True or False. Determines whether "ese" under the simple distance model is a uniform distribution (if set to True), or rather centred around the medial objects (if set to False)
+
 output_dict = {"Model":[],
 			   "Word":[],
 			   "Cost":[],
@@ -31,7 +33,7 @@ output_dict = {"Model":[],
 			   "ListenerTau":[]}
 ########################################################################################
 
-Model = Speaker_MW.Speaker(output_dict, stau=speaker_rationality, ltau=listener_rationality, verbose=False)
+Model = Speaker_MW.Speaker(output_dict, stau=speaker_rationality, ltau=listener_rationality, verbose=False, uniform_ese=ese_uniform)
 
 # Model.PrintHeader()  # MW: Got rid of this, because everything is being saved to a pandas dataframe now.
 
@@ -40,7 +42,7 @@ for listener_rationality in np.arange(tau_start, tau_stop, tau_step):
 	# print(f"listener_rationality is {listener_rationality}:")
 	for speaker_rationality in np.arange(tau_start, tau_stop, tau_step):
 		# print(f"speaker_rationality is {speaker_rationality}:")
-		Model = Speaker_MW.Speaker(output_dict, stau=speaker_rationality,ltau=listener_rationality,verbose=False)
+		Model = Speaker_MW.Speaker(output_dict, stau=speaker_rationality,ltau=listener_rationality,verbose=False, uniform_ese=ese_uniform)
 		for model in models:
 			for latt in [0,1,2,3]:
 				for referent in [1,2,3]:  # If I understood the design correctly, Exp. 2 only uses object positions [1, 2, 3]
@@ -61,7 +63,7 @@ pd.set_option('display.max_columns', None)
 
 # output_file_path = '/Users/U968195/PycharmProjects/demonstratives_model/model_predictions/'
 output_file_path = 'model_predictions/'
-output_file_name = 'HigherSearchD_MW_Simple_Attention_'+ str(models).replace(" ", "") + '_tau_start_'+str(tau_start)+'_tau_stop_'+str(tau_stop)+'_tau_step_'+str(tau_step)+'.csv'
+output_file_name = 'HigherSearchD_MW_Simple_Attention_Ese_uniform_'+ str(ese_uniform) + '_' + str(models).replace(" ", "") + '_tau_start_'+str(tau_start)+'_tau_stop_'+str(tau_stop)+'_tau_step_'+str(tau_step)+'.csv'
 output_dataframe.to_csv(output_file_path+output_file_name, index=False)
 
 
